52fe7b0643e8d83cf25b2136dd016c7cefc2f5ac
mrmtshmp <mr.mt.shm@gmail.com>
2019-08-10 16:05:44 +0900

    add a function

--- R/dist_and_extract.R
+++ R/dist_and_extract.R
@@ -17,6 +17,7 @@
 
 dist_and_extract <- function(
   data,
+  sufix = "extracted",
   tree_phylo=NULL,
   method="euclidean",
   type_extr="Type%in%c(3,4)",
@@ -24,6 +25,10 @@ dist_and_extract <- function(
   param.UniFrac= c(0, 0.5, 1.0, "UW") # -1 for unweighted UniFrac
   )
   {
+  if(method=="UniFrac" & is.null(tree_phylo)){
+    stop("Unifrac needs phylogenic tree")
+    }
+
   OK_or_NoWay <-
     readline(
       "This function might overwrite objects in GlovalEnv. Are you OK? 'OK' or 'NW'"
@@ -34,45 +39,45 @@ dist_and_extract <- function(
 
   cat("OK\n")
 
-  otutable_on_taxonomy <- data
+  .otutable_on_taxonomy <- data
 
   .method    <- method
   .type_extr <- type_extr
   .Name_type <- Name_type
 
-  # if(length(which(param.UniFrac %in% "UW")>0)){
-  #   .param.UniFrac <- as.numeric(
-  #     param.UniFrac[
-  #       -which(
-  #         param.UniFrac %in% "UW"
-  #         )
-  #       ])
-  #     .UW <- TRUE
-  #     } else {
+
+   if(length(which(param.UniFrac %in% "UW")>0)){
+      .param.UniFrac <- as.numeric(
+        param.UniFrac[
+          -which(
+            param.UniFrac %in% "UW"
+            )
+          ])
+        .UW <- TRUE
+        print(.param.UniFrac)
+        } else {
          .param.UniFrac <- param.UniFrac
-  #       .UW <- FALSE
-  #       }
+          .UW <- FALSE
+          print(.param.UniFrac)
+          }
 
-
-
-  if(.method=="UniFrac" & is.null(tree_phylo)){
-    stop("Unifrac needs phylogenic tree")
-    }
 
   if(
     .method=="UniFrac" &
     length(.param.UniFrac) > 0
     ){
     unifrac_relative <- GUniFrac(
-      otutable_on_taxonomy,
+      .otutable_on_taxonomy,
       tree  = tree_phylo,
       alpha = .param.UniFrac
       )
     for(i in 1:length(.param.UniFrac)){
       assign(
         sprintf(
-          "d%s_relative",
-          .param.UniFrac[i]),
+          "d%s_%s",
+          .param.UniFrac[i],
+          sufix
+          ),
         unifrac_relative[[1]][
           ,,
           sprintf("d_%s", .param.UniFrac[i])
@@ -81,21 +86,25 @@ dist_and_extract <- function(
         )
       }
     }
-
+  print("hello1")
   if(
     .method %in% c("bray", "jaccard")
     ){
+
+    res.dist <- vegdist(
+      .otutable_on_taxonomy,
+      method = .method
+      ) %>%
+      extract_type(
+        extract_type,
+        df.Name_type
+        )
+    print("hello2")
+
     assign(
       sprintf("d%s_vegan", .method),
-      vegan::vegdist(
-        otutable_on_taxonomy,
-        method = .method
-        ) %>%
-        extract_type(
-          extract_type,
-          df.Name_type
-          )
-    )
+      res.dist
+      )
   }
 }
 
