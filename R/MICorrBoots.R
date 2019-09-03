5266d14d167c0477751f76f5aaeb64a0fc74b53f
mrmtshmp <mr.mt.shm@gmail.com>
2019-09-03 17:35:09 +0900

    add a function

--- R/MICorrBoots.R
+++ R/MICorrBoots.R
@@ -39,7 +39,6 @@
 #'
 #' @export
 
-
 MICorrBoots <- function(
   X,
   Y,
@@ -54,7 +53,7 @@ MICorrBoots <- function(
     rep(
       1/length(unique(ADS$subjID)),
       length(unique(ADS$subjID))
-      ),
+    ),
   ...
 ){
 
@@ -63,79 +62,106 @@ MICorrBoots <- function(
   df.pmt <- data.frame(
     i=seq(1:n.sim),
     j=seq(1:n.sim)
-    )
+  )
 
 
-    if(
-      method !="MIC" &
-      disc.X == "none" &
-      disc.Y == "none"
-    ){
-      output <- ddply(
-        df.pmt,
-        .(i),
-        function(itt){
-          i.numb <- unique(itt$i)
+  if(
+    method !="MIC" &
+    disc.X == "none" &
+    disc.Y == "none"
+  ){
+    output <- ddply(
+      df.pmt,
+      .(i),
+      function(itt){
+        i.numb <- unique(itt$i)
 
-          if(i.numb > 1){
-            D.boot <- data.frame()
-            D.boot <- ddply(
-              D,
-              .(S),
-              function(d){
-                unique.subj.D <- unique(D$S)
-                D.boot <-
-                  D[
-                    D$S==
-                      unique.subj.D[
-                        sample(
-                          size = 1,
-                          x = 1:length(unique.subj.D),
-                          prob = samp.prob,
-                          replace=TRUE
-                          )
-                        ],
-                    ]
-                D.boot$ori.ID <- unique(d$S)
-                return(D.boot)
-              }
-            )
-          } else {
-            D.boot <- D
-            D.boot$ori.ID <- D$S
-          }
-
-
-          X=as.factor(D.boot$X)
-          Y=as.factor(D.boot$Y)
-          S=as.factor(D.boot$ori.ID)
-          df.test <- data.frame(X,Y,S)
-            m1a <- infotheo::condinformation(
-              X=X,
-              Y=Y,
-              S=S,
-              method = method
-            )/ infotheo::entropy(unique(X, Y, S, method=method))
-
-          return(m1a)
-        }
-      )
-    }else{
-      output <- ddply(
-        df.pmt,
-        .(i),
-        function(itt){
-          i.numb <- unique(itt$i)
-          if(i.numb > 1) D$Y <- sample(D$Y)
-          m1a <- minerva::mine(
-            x=(D$X),
-            y=(D$Y),
-            alpha = alpha,
-            ...
+        if(i.numb > 1){
+          D.boot <- data.frame()
+          D.boot <- ddply(
+            D,
+            .(S),
+            function(d){
+              unique.subj.D <- unique(D$S)
+              D.boot <-
+                D[
+                  D$S==
+                    unique.subj.D[
+                      sample(
+                        size = 1,
+                        x = 1:length(unique.subj.D),
+                        prob = samp.prob,
+                        replace=TRUE
+                      )
+                      ],
+                  ]
+              D.boot$ori.ID <- unique(d$S)
+              return(D.boot)
+            }
           )
-          return(c(m1a$MIC, m1a$MAS, m1a$MEV, m1a$MCN, m1a$`MIC-R2`, m1a$GMIC, m1a$TIC))
+        } else {
+          D.boot <- D
+          D.boot$ori.ID <- D$S
         }
-      )
-    }
+
+
+        X=factor(D.boot$X, levels=unique(c(D.boot$X, D.boot$Y)))
+        Y=factor(D.boot$Y, levels=unique(c(D.boot$X, D.boot$Y)))
+        print(sprintf("%s_%s,%s",length(X),length(Y),length(as.factor(D.boot$ori.ID))))
+        S=as.factor(D.boot$ori.ID)
+        m1a_1 <-
+          infotheo::condinformation(
+            X=X,
+            Y=Y,
+            #              S=S,
+            method = method
+          )
+        print("?")
+
+        print(unique(c(X,Y)))
+        print(X)
+        print(Y)
+
+        m1a_2 <-
+          infotheo::condentropy(
+            unique(X, Y),
+            method=method
+          )
+
+        print("??")
+        print(sprintf("condinfo_%s",length(m1a_1)))
+        print(sprintf("condentr_%s",length(m1a_2)))
+
+        m1a <- m1a_1/ m1a_2
+
+        vec.consis <-
+          data.frame(X=X,Y=Y) %>%
+          mutate(
+            consis= ifelse(X==Y, 1, 0)
+          )
+
+        consis <- mean(vec.consis$consis)
+
+        return(m1a*consis)
+      }
+    )
+  }else{
+    output <- ddply(
+      df.pmt,
+      .(i),
+      function(itt){
+        i.numb <- unique(itt$i)
+        if(i.numb > 1) D$Y <- sample(D$Y)
+        m1a <- minerva::mine(
+          x=(D$X),
+          y=(D$Y),
+          alpha = alpha,
+          ...
+        )
+        return(c(m1a$MIC, m1a$MAS, m1a$MEV, m1a$MCN, m1a$`MIC-R2`, m1a$GMIC, m1a$TIC))
+      }
+    )
+  }
+
   return(output)
 }
